1f37ffb39561074b4f179b6f517e7ed783efa561
DimaPigul <134810722+DimaPigul@users.noreply.github.com>
2023-06-03 23:45:26 +0200

    Add files via upload

--- bot.py
+++ bot.py
@@ -4,6 +4,8 @@ import requests
 import telebot
 
 from seleniumbase import Driver
+import mysql.connector
+from mysql.connector import Error
 
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options  
@@ -24,6 +26,8 @@ class User:
 
 bot = telebot.TeleBot(BOT_TOKEN)
 
+#Declaration of functions
+##Basic functions
 def day_handler(message):
     sign = message.text
     text = "What day do you want to know?\nChoose one: *TODAY*, *TOMORROW*, *YESTERDAY*, or a date in format YYYY-MM-DD."
@@ -54,8 +58,8 @@ def fetch_horoscope(message, sign):
     bot.send_message(message.chat.id, horoscope_message, parse_mode="Markdown")
 
 def duwcheck(message):
-    if str(message.chat.id) != '1231454527':
-        return bot.send_message(message.chat.id, "Sorry, you do not have rights")
+    if str(message.from_user.username) != 'dimapiggy':
+        return bot.send_message(message.chat.id, "Sorry, you do not have access")
     bot.send_message(message.chat.id, "Thank you, sir. 1 minute, please")
 
     #chrome_options = Options()  
@@ -92,6 +96,176 @@ def duwcheck(message):
     os.remove("screenshot.png")
     bot.send_message(message.chat.id, "Process finished, screenshot removed")
 
+##SQL functions
+def create_connection(host_name, user_name, user_password):
+    connection = None
+    try:
+        connection = mysql.connector.connect(
+            host=host_name,
+            user=user_name,
+            passwd=user_password,
+        )
+        print("Connection to MySQL DB successful")
+    except Error as e:
+        print(f"The error '{e}' occurred")
+    return connection
+
+def create_connection_todb(host_name, user_name, user_password, db_name):
+    connection = None
+    try:
+        connection = mysql.connector.connect(
+            host=host_name,
+            user=user_name,
+            passwd=user_password,
+            database=db_name
+        )
+        print("Connection to MySQL DB successful")
+    except Error as e:
+        print(f"The error '{e}' occurred")
+
+    return connection
+
+def create_database(connection, query):
+    cursor = connection.cursor()
+    try:
+        cursor.execute(query)
+        print("Database created successfully")
+    except Error as e:
+        print(f"The error '{e}' occurred")
+
+def execute_query(connection, query):
+     cursor = connection.cursor()
+     try:
+         cursor.execute(query)
+         connection.commit()
+         print("Query executed successfully")
+     except Error as e:
+         print(f"The error '{e}' occurred")
+
+def execute_read_query(connection, query):
+    cursor = connection.cursor()
+    result = None
+    try:
+        cursor.execute(query)
+        result = cursor.fetchall()
+        return result
+    except Error as e:
+        print(f"The error '{e}' occurred")
+
+##functions for order
+
+def Order_size(message):
+    Product = message.text
+    text = "How many of this do you want?"
+    sent_msg = bot.send_message(message.chat.id, text, parse_mode="Markdown")
+    bot.register_next_step_handler(sent_msg, Order_shipping, Product)
+
+def Order_shipping(amount,Product):
+    text = "Where we need to ship it?"
+    sent_msg = bot.send_message(amount.chat.id, text, parse_mode="Markdown")
+    bot.register_next_step_handler(sent_msg, Order_placement, Product,amount.text)
+def Order_placement(shipping_adresse,Product,amount):
+    select_user = "SELECT id FROM users WHERE userchatnumber={}".format(shipping_adresse.chat.id)
+    user_to_show = execute_read_query(connection, select_user)
+    if len(user_to_show)==0:
+        bot.send_message(shipping_adresse.chat.id, "Something went wrong")
+    else:
+        user_to_show[0]=list(user_to_show[0])
+        user_id=int(user_to_show[0][0])
+        sql = "INSERT INTO orders (user_id, product, amount,shipping_adresse) VALUES ( %s, %s, %s, %s )"
+        val = [(user_id, str(Product), int(amount),str(shipping_adresse.text))]
+        cursor = connection.cursor()
+        cursor.executemany(sql, val)
+        connection.commit()
+        bot.send_message(shipping_adresse.chat.id, "Thank you for your oder, it was successfully placed")
+
+#Creating Database
+connection = create_connection("telebot1-388021:europe-central2:products", "root", "")
+
+
+create_database_query = "CREATE DATABASE IF NOT EXISTS products"
+create_database(connection, create_database_query)
+
+connection = create_connection_todb("telebot1-388021:europe-central2:products", "root", "", "products")
+
+create_products_list_table = """
+CREATE TABLE IF NOT EXISTS products_list (
+  id INT NOT NULL AUTO_INCREMENT, 
+  name TEXT NOT NULL, 
+  price FLOAT(2), 
+  image TEXT, 
+  awailability BOOL, 
+  PRIMARY KEY (id)
+) ENGINE = InnoDB
+"""
+execute_query(connection, create_products_list_table)
+create_amount_column="""ALTER TABLE products_list
+ADD COLUMN IF NOT EXISTS amount INT NOT NULL AFTER image;"""
+execute_query(connection, create_amount_column)
+
+create_users_list_table = """
+CREATE TABLE IF NOT EXISTS users (
+  id INT NOT NULL AUTO_INCREMENT, 
+  username TEXT NOT NULL, 
+  userchatnumber INT NOT NULL,
+  numberoforders INT NOT NULL, 
+  PRIMARY KEY (id)
+) ENGINE = InnoDB
+"""
+execute_query(connection, create_users_list_table)
+
+create_orders_list_table = """
+CREATE TABLE IF NOT EXISTS orders (
+  id INT NOT NULL AUTO_INCREMENT, 
+  user_id INT NOT NULL, 
+  product TEXT NOT NULL,
+  amount INT NOT NULL,
+  shipping_adresse TEXT NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (user_id) 
+      REFERENCES users (id)
+) ENGINE = InnoDB
+"""
+execute_query(connection, create_orders_list_table)
+
+
+#Bot operations
+## Operations for users
+@bot.message_handler(commands=['allproducts'])
+def Allproducts(message):
+    select_products = "SELECT name,price,awailability FROM products_list"
+    products_to_show = execute_read_query(connection, select_products)
+    for i in range(len(products_to_show)):
+        products_to_show[i]=list(products_to_show[i])
+
+    for k in range(len(products_to_show)):
+        for j in range(len(products_to_show[k])):
+            products_to_show[k][j]=str(products_to_show[k][j])
+    for i in range(len(products_to_show)):
+        if products_to_show[i][2]=="1":
+            text_of_the_message="{}, price={}, is awailable".format(products_to_show[i][0],products_to_show[i][1])
+            bot.send_message(message.chat.id, text_of_the_message, parse_mode="Markdown")
+        else:
+            text_of_the_message="{}, price={}, is not currently awailable".format(products_to_show[i][0],products_to_show[i][1])
+            bot.send_message(message.chat.id, text_of_the_message, parse_mode="Markdown")
+
+@bot.message_handler(commands=['awailableproducts'])
+def Awailableproducts(message):
+    select_products = "SELECT name,price,awailability FROM products_list WHERE awailability=1"
+    products_to_show = execute_read_query(connection, select_products)
+    for i in range(len(products_to_show)):
+        products_to_show[i]=list(products_to_show[i])
+
+    for k in range(len(products_to_show)):
+        for j in range(len(products_to_show[k])):
+            products_to_show[k][j]=str(products_to_show[k][j])
+    for i in range(len(products_to_show)):
+        if products_to_show[i][2]==1:
+            text_of_the_message="{}, price={}, is awailable".format(products_to_show[i][0],products_to_show[i][1])
+            bot.send_message(message.chat.id, text_of_the_message, parse_mode="Markdown")
+        else:
+            text_of_the_message="{}, price={}, is not currently awailable".format(products_to_show[i][0],products_to_show[i][1])
+            bot.send_message(message.chat.id, text_of_the_message, parse_mode="Markdown")
 
 @bot.message_handler(commands=['hello'])
 def greetings(message):
@@ -99,7 +273,7 @@ def greetings(message):
 
 @bot.message_handler(commands=['id'])
 def id(message):
-    bot.send_message(message.chat.id, str(message.chat.id))
+    bot.send_message(message.chat.id, str(message.from_user.username))
 
 @bot.message_handler(commands=['me'])
 def simple_response(message):
@@ -107,7 +281,17 @@ def simple_response(message):
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message, "Howdy, how are you doing?")
+    select_users = "SELECT userchatnumber FROM users WHERE userchatnumber={}".format(message.chat.id)
+    users_to_show = execute_read_query(connection, select_users)
+    if len(users_to_show)==0:
+        sql = "INSERT INTO users (username, userchatnumber, numberoforders) VALUES ( %s, %s, %s )"
+        val = [(str(message.from_user.username), message.chat.id, 0)]
+        cursor = connection.cursor()
+        cursor.executemany(sql, val)
+        connection.commit()
+        bot.reply_to(message, "Hey, I did not seen you before!")
+    else:
+        bot.reply_to(message, "Hey, I could recognize you!")
 
 @bot.message_handler(commands=['horoscope'])
 def sign_handler(message):
@@ -121,6 +305,12 @@ def Checkingduw(message):
     #sent_msg = bot.send_message(message.chat.id, text, parse_mode="Markdown")
     #bot.register_next_step_handler(sent_msg, duwcheck)
     duwcheck(message)
+
+@bot.message_handler(commands=['order'])
+def placeorder(message):
+    text = "Please, Choose product from awailable products, you want to order:"
+    sent_msg = bot.send_message(message.chat.id, text, parse_mode="Markdown")
+    bot.register_next_step_handler(sent_msg, Order_size)
 
 @bot.message_handler(func=lambda msg: True)
 def echo_all(message):
